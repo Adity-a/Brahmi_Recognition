28fdb5feea325946e2ca4a910618d33d59104691
Adity-a <adityabalodi31@gmail.com>
2024-05-10 11:07:45 +0530

    added opencv

---
+++ sdk/build/intermediates/cxx/Debug/15333n28/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\adity\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\adity\\StudioProjects\\Major-Project\\sdk\\.cxx\\Debug\\15333n28\\arm64-v8a" ^
+  opencv_jni_shared
