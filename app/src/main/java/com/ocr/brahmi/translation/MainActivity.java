69c91833b1a5edd8592cda01690c0408c6272b0b
Adity-a <adityabalodi31@gmail.com>
2024-05-15 12:51:22 +0530

    Comments

--- app/src/main/java/com/ocr/brahmi/translation/MainActivity.java
+++ app/src/main/java/com/ocr/brahmi/translation/MainActivity.java
@@ -640,6 +640,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        moveTaskToBack(true);git status
+        moveTaskToBack(true); // Move the task containing this activity to the background
     }
 }
